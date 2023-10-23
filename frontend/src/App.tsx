6f45659d874fd5b3ce15c1f3239226cbfb61f030
Isaac <isaacagatep@gmail.com>
2023-10-23 17:55:46 +0800

    fixed app routing in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import Login from './pages/Login';
 import ForgetPassword from './pages/ForgetPassword';
+import ResetPassword from './pages/ResetPassword';
 
 export default function App() {
     return (
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="forgetPassword" element={<ForgetPassword />}/>
+        <Route path="resetPassword/:resetToken" element={<ResetPassword />}/>
       </Routes>
     </div>
     );
